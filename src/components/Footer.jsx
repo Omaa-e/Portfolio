53e475b83e26aeb4b88482d78b15050d0f946f2e
meune <ekpeterechioma@gmail.com>
2025-10-20 17:29:30 -0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center py-4 mx-auto">
+        <p>&copy; Chioma Ekpetere, {new Date().getFullYear()}</p>
+    </div>
+  )
+}
+
+export default Footer
